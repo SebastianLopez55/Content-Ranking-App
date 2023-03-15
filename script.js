61325fd7ba8d797b9e4591d45b1a2088a34caea1
SebastianLopez55 <lopezsebastian856@gmail.com>
2023-03-14 23:29:20 -0700

    refactor(forder): change folder structure

--- script.js
+++ script.js
@@ -122,35 +122,3 @@ btn.addEventListener("click", function () {
 
 console.log([7, 64, 6, -23, 11].filter((el) => el > 10));
 console.log([7, 64, 6, -23, 11].find((el) => el > 10));
-
-// ==== JS Basics ====
-
-// == Ternary operator: helpful because it produces a value ==
-// const result =
-//   condition1 > condition2 ? "return value if true" : "return value if false";
-
-// == Arrow Functions ==
-// const add = (a, b) => a + b;
-// Function call: add(1, 2);
-
-// == Arrays In JS ==
-// const numbers = [1, 2, 3, "Hi", true];
-// const [first, second, third, string, boolean] = numbers;
-// consol.log(first) will give 1
-// Unpacking arrays: const [first, second, ...rest] = numbers;
-
-// == Looping over arrays ==
-// cost arr = [1, 2, 3, 4, 5];
-// arr.forEach( function (element) { console.log(element) } );
-// USING THE MAP METHODS
-// const newArr = arr.map( (element) => element * 2 ); // [2, 4, 6, 8, 10]
-
-// == Objects In JS ==
-// const objectTest = {
-//   name: "John",
-//   age: 30,
-//   returnAge: function () {
-//     return this.age;
-//   },
-// };
-// console.log(objectTest.returnAge()); // 30

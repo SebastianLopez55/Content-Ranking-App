bf119f257397be0095c4e5f3208730bec857a181
SebastianLopez55 <lopezsebastian856@gmail.com>
2023-03-15 08:59:24 -0700

    refactor(react): add components to react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 // Description: This is the main component of the app.
 //  It is the first component that is rendered when the app is loaded.
 
+import "./style.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>Today I Learned</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+function NewFactForm() {
+  return <form className="fact-form"> Fact Form </form>;
+}
+
+function CategoryFilter() {
+  return <aside> Category Filter </aside>;
+}
+
+function FactList() {
+  return <section> Fact List </section>;
 }
 
 export default App;
